30c04712719f1e4b9e6f2899b3c6a1d9e999f18a
Tao Xu <wondertx@gmail.com>
2015-01-08 17:19:54 +0800

    upload procedures

---
+++ sql/procedure/postgres/is_junction_of_highway(bigint).sql
@@ -0,0 +1,39 @@
+﻿-- Function: taxi.is_junction_of_highway(bigint)
+
+-- DROP FUNCTION taxi.is_junction_of_highway(bigint);
+
+CREATE OR REPLACE FUNCTION taxi.is_junction_of_highway(v_node_id bigint)
+  RETURNS boolean AS
+$BODY$
+declare
+	result boolean default false;
+	count int;
+begin
+	select
+		count(distinct way_id)
+	from
+		way_nodes
+	where
+		node_id = v_node_id and 
+		exists (
+			select * 
+			from
+				ways
+			where
+				id = way_id and 
+				tags ? 'highway'
+		)
+	into
+		count
+	;
+	if count > 1 then 
+		result := true;
+	end if
+	;
+	return result;
+end;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+ALTER FUNCTION taxi.is_junction_of_highway(bigint)
+  OWNER TO tx;
---
+++ sql/procedure/postgres/temp.sql
@@ -0,0 +1,41 @@
+﻿select * from ways where tags ? 'highway' and tags->'name'='江东中路' fetch first 10 rows only;
+--checking sections
+select taxi.generate_sections_segments();
+select
+	T2.way_id , T1.*, T3.nodes
+from 
+	taxi.sections T1,
+	taxi.section_way T2,
+	ways T3
+where
+	T1.id = T2.section_id and
+	T2.way_id = T3.id
+;
+--checking segments
+select
+	T4.id,  T1.*, T4.nodes
+from
+	taxi.segments T1,
+	taxi.segment_section T2,
+	taxi.section_way T3,	
+	ways T4
+where
+	T1.id = T2.segment_id and
+	T2.section_id = T3.section_id and
+	T3.way_id = T4.id
+;
+select 
+	T2.id from_id, T3.id to_id,
+	st_distance_sphere(T2.geom, T3.geom) distance
+from 
+	taxi.segments T1,
+	nodes T2,
+	nodes T3
+where
+	T1.from_node = T2.id and
+	T1.to_node = T3.id
+order by
+	3 desc
+;
+select st_x(geom) from nodes fetch first 3 rows only;
+	
