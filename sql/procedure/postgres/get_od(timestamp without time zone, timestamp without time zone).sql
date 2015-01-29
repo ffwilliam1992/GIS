13be737d3eaff87259289b7f3cea8d03d52521ed
Tao Xu <wondertx@gmail.com>
2015-01-29 11:43:03 +0800

    fix error in D points

--- sql/procedure/postgres/get_od(timestamp without time zone, timestamp without time zone).sql
+++ sql/procedure/postgres/get_od(timestamp without time zone, timestamp without time zone).sql
@@ -7,11 +7,10 @@ CREATE OR REPLACE FUNCTION taxi.get_od(
     v_end timestamp without time zone DEFAULT now())
  RETURNS setof record AS
 $BODY$
+declare v_num1 bigint;
+declare v_num2 bigint;
 begin
-	return query
-	select
-		T1.id, T1.point, T1.timestamp, T2.point, T2.timestamp
-	from (
+	create temp table T1 on commit drop as
 		select
 			row_number() over(partition by id), *
 		from (
@@ -34,51 +33,53 @@ begin
                 timestamp
 			)
 		)as T
-    where
-        lead and
-        not lag and
-        state   
-    )as T1, (
-    select
-        row_number() over(partition by id) ,*
-    from (
-        select
-            lead(state) over w,
-            lag(state) over w,
-            state,
-            point, id,
-            st_x(point) longitude,
-            st_y(point) latitude,
-            timestamp
-        from
-            taxi.gps_raw
-        where
-            timestamp between v_begin and v_end
-        window w as (
-            partition by
-                id
-            order by
-                timestamp
-        )
-    )as T
-    where
-        not lead and
-        lag and
-        state   
-    )as T2
-	where
-		T1.id = T2.id and
-		T1.lead and
-		not T1.lag and
-		T1.state and
-		not T2.lead and
-		T2.lag and
-		T2.state and
-		T1.row_number = T2.row_number
-;
+		where
+			lead and
+			not lag and
+			state
+     ;
+    create temp table T2 on commit drop as
+		select
+			row_number() over(partition by id), *
+		from (
+			select
+				lead(state) over w,
+				lag(state) over w,
+				state,
+				point, id,
+				st_x(point) longitude,
+				st_y(point) latitude,
+				timestamp
+			from
+				taxi.gps_raw
+			where
+				timestamp between v_begin and v_end
+			window w as (
+				partition by
+					id
+				order by
+					timestamp
+			)
+		)as T
+		where
+			not lead and
+			lag and
+			state and
+			timestamp > (select min(timestamp) from T1 where id = T1.id)
+    ;
+
+    return query
+		select 
+			T1.id, T1.point, T1.timestamp, T2.point, T2.timestamp
+		from
+			T1 inner join T2 on
+				T1.row_number = T2.row_number and 
+				T1.id = T2.id
+	;
+    
 end;
 $BODY$
-  LANGUAGE plpgsql IMMUTABLE STRICT
+  LANGUAGE plpgsql
   COST 100;
 ALTER FUNCTION taxi.get_od(timestamp without time zone, timestamp without time zone)
   OWNER TO tx;
