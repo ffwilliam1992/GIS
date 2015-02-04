e0f6730f0fcfaadbf3425587d266889fc5d19c84
Tao Xu <wondertx@gmail.com>
2015-02-04 17:37:24 +0800

    procedures for augmented route

---
+++ sql/procedure/postgres/get_nearest_vertex(geometry, double precision, double precision).sql
@@ -0,0 +1,32 @@
+﻿-- Function: taxi.get_nearest_vertex(geometry, double precision, double precision)
+
+-- DROP FUNCTION taxi.get_nearest_vertex(geometry, double precision, double precision);
+
+CREATE OR REPLACE FUNCTION taxi.get_nearest_vertex(
+    v_point geometry,
+    v_x double precision,
+    v_y double precision)
+  RETURNS bigint AS
+$BODY$
+declare v_id bigint;
+begin
+	select
+		id
+	from
+		taxi.edges_vertices_pgr
+	where
+		(st_x(the_geom)-st_x(v_point))*v_x
+		+(st_y(the_geom)-st_y(v_point))*v_y > 0
+	order by
+		st_distance_sphere(v_point, the_geom)
+	limit 1
+	into v_id
+	;
+	return v_id
+	;
+end;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE STRICT
+  COST 100;
+ALTER FUNCTION taxi.get_nearest_vertex(geometry, double precision, double precision)
+  OWNER TO tx;
---
+++ sql/procedure/postgres/get_trip_traces(bigint).sql
@@ -0,0 +1,52 @@
+﻿-- Function: taxi.get_trip_traces(bigint)
+
+-- DROP FUNCTION taxi.get_trip_traces(bigint);
+
+CREATE OR REPLACE FUNCTION taxi.get_trip_traces(v_trip_id bigint)
+  RETURNS SETOF bigint AS
+$BODY$
+declare v_pre record;
+declare v_trace record;
+declare v_pre_vertex bigint := -1;
+declare v_cur_vertex bigint;
+declare v_vertex bigint;
+declare v_pgr_cost_result record;
+begin
+	select taxi.get_trip_vertices(v_trip_id)
+	into v_pre_vertex 
+	limit 1
+	;
+	for v_vertex in
+	select *
+	from
+		(select taxi.get_trip_vertices(v_trip_id) v) T
+	where
+		v <> v_pre_vertex
+	loop
+		if(taxi.is_adjacent(v_pre_vertex, v_vertex)) then
+			return query select id from taxi.edges where source = v_pre_vertex and target = v_vertex;
+		else
+			for v_pgr_cost_result in
+			select seq, id1, id2, cost from pgr_dijkstra(
+				'select id, source::integer, target::integer, len as cost, rlen reverse_cost from taxi.edges',
+				v_pre_vertex::integer,
+				v_vertex::integer,
+				true,
+				true
+			)
+			loop
+				if(v_pgr_cost_result.id2 <> -1) then
+					return next v_pgr_cost_result.id2;
+				end if
+				;
+			end loop;
+		end if;
+		v_pre_vertex = v_vertex;
+	end loop;
+end;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE STRICT
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION taxi.get_trip_traces(bigint)
+  OWNER TO tx;
