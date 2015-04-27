4cf326239989563c095aededc01f57e11b9d242a
Tao Xu <henry.tao@sap.com>
2015-04-27 15:48:02 +0800

    routes BETWEEN and ACROSS OD

---
+++ sql/procedure/postgres/get_trips().sql
@@ -0,0 +1,52 @@
+﻿-- Function: taxi.get_trips(character varying)
+
+-- DROP FUNCTION taxi.get_trips(character varying);
+--This procedure filles the field trip_id of taxi.gps_raw
+CREATE OR REPLACE FUNCTION taxi.get_trips()
+  RETURNS void AS
+$BODY$
+declare v_record record;
+declare v_pre_state boolean default false;
+declare v_trip_id bigint default 0;
+declare	v_cur no scroll cursor for
+		select 
+			id, state, timestamp
+		from
+			taxi.gps_raw
+		order by
+			id, timestamp
+	for update
+;
+begin
+	for v_record in v_cur
+	loop
+		--continuous uncarried
+		if(not v_pre_state and not v_record.state) then
+			continue;
+		end if;
+		--carried to uncarried
+		if(v_pre_state and not v_record.state) then
+			v_pre_state = false;
+			continue;
+		end if;
+		--uncarried to carried
+		if(not v_pre_state and v_record.state) then
+			v_trip_id = v_trip_id + 1;
+			v_pre_state = true;
+		end if;
+
+		update 
+			taxi.gps_raw
+		set 
+			trip_id = v_trip_id
+		where
+			current of v_cur
+		;
+	end loop;
+end;
+$BODY$
+  LANGUAGE plpgsql
+  COST 100
+;
+ALTER FUNCTION taxi.get_trips()
+  OWNER TO tx;
---
+++ sql/procedure/postgres/get_trips_with_od(double precision, double precision, double precision, double precision, double precision).sql
@@ -0,0 +1,42 @@
+﻿-- Function: taxi.get_trips_with_od(double precision, double precision, double precision, double precision, double precision)
+
+-- DROP FUNCTION taxi.get_trips_with_od(double precision, double precision, double precision, double precision, double precision);
+
+CREATE OR REPLACE FUNCTION taxi.get_trips_with_od(
+    v_lng1 double precision,
+    v_lat1 double precision,
+    v_lng2 double precision,
+    v_lat2 double precision,
+    v_radius double precision DEFAULT 100)
+  RETURNS SETOF bigint AS
+$BODY$
+begin
+	return query
+	select
+		trip_id
+	from(
+		select distinct
+			first_value(point) over w as s,
+			last_value(point) over w as e,
+			trip_id
+		from
+			taxi.gps_raw
+		window w as (
+			partition by
+				trip_id
+			order by
+				timestamp
+			range between UNBOUNDED PRECEDING and UNBOUNDED FOLLOWING
+		)) as T
+	where
+		trip_id is not null and
+		st_distance_sphere(s, st_point(v_lng1, v_lat1)) < v_radius and
+		st_distance_sphere(e, st_point(v_lng2, v_lat2)) < v_radius
+	;
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION taxi.get_trips_with_od(double precision, double precision, double precision, double precision, double precision)
+  OWNER TO tx;
